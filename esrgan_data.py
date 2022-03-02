3c7e6bb7e9e2d954a87c535c80416d0f0c4fdde8
final-0 <99001296+final-0@users.noreply.github.com>
2022-03-02 11:11:24 +0900

    Update esrgan_data.py

--- esrgan_data.py
+++ esrgan_data.py
@@ -55,7 +55,7 @@ class RealESRGANDataset(Dataset):
         # crop or pad to 400
         # TODO: 400 is hard-coded. You may change it accordingly
         h, w = img_gt.shape[0:2]
-        crop_pad_size = 1024
+        crop_pad_size = image_size
         # pad
         if h < crop_pad_size or w < crop_pad_size:
             pad_h = max(0, crop_pad_size - h)
